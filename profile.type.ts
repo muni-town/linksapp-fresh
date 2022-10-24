4250406673343d0b71ab6e9b32dc5c564eaa679b
Arthur <19251998+arthur-ver@users.noreply.github.com>
2022-10-24 19:52:18 +0200

    Create profile.type.ts

---
+++ profile.type.ts
@@ -0,0 +1,29 @@
+type Profile = {
+  avatar: string;
+  username: string;
+  bio: string;
+  location?: string;
+  readme?: string;
+  socialAccounts: {
+    dribbble?: string;
+    facebook?: string;
+    github?: string;
+    instagram?: string;
+    linkedin?: string;
+    twitter?: string;
+    youtube?: string;
+    website?: string;
+    mail?: string;
+  };
+  announcement?: {
+    title: string;
+    text: string;
+  };
+  links: {
+    url: string;
+    title: string;
+  }[];
+  rss?: string;
+};
+
+export default Profile;
