1596a292f86a00d14592853b1aea160bf4d27007
harshmangalam <harshdev8218@gmail.com>
2022-10-25 10:14:33 +0530

    added github features

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,13 +3,13 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/github.tsx";
+import * as $0 from "./routes/github/[username]/index.tsx";
 import * as $1 from "./routes/index.tsx";
 import * as $$0 from "./islands/TabsIsland.tsx";
 
 const manifest = {
   routes: {
-    "./routes/github.tsx": $0,
+    "./routes/github/[username]/index.tsx": $0,
     "./routes/index.tsx": $1,
   },
   islands: {
