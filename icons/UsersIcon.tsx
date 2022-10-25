d517d27d4a7e2047eac7053bfa170afaa4ff1f2a
harshmangalam <harshdev8218@gmail.com>
2022-10-25 10:12:58 +0530

    added users icon

---
+++ icons/UsersIcon.tsx
@@ -0,0 +1,18 @@
+export default function UsersIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+      />
+    </svg>
+  );
+}
