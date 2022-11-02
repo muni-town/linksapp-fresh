265470e5b111703771759dc23f22d3dcd87286b1
harshmangalam <harshdev8218@gmail.com>
2022-11-02 15:38:57 +0530

    added github followers route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,16 +3,18 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/github/[username]/index.tsx";
-import * as $1 from "./routes/github/[username]/repositories.tsx";
-import * as $2 from "./routes/index.tsx";
+import * as $0 from "./routes/github/[username]/followers.tsx";
+import * as $1 from "./routes/github/[username]/index.tsx";
+import * as $2 from "./routes/github/[username]/repositories.tsx";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/TabsIsland.tsx";
 
 const manifest = {
   routes: {
-    "./routes/github/[username]/index.tsx": $0,
-    "./routes/github/[username]/repositories.tsx": $1,
-    "./routes/index.tsx": $2,
+    "./routes/github/[username]/followers.tsx": $0,
+    "./routes/github/[username]/index.tsx": $1,
+    "./routes/github/[username]/repositories.tsx": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/TabsIsland.tsx": $$0,
